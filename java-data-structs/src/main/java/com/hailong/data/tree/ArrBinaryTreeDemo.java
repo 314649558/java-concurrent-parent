a345a1a92af664ff03bdd121746b3f8434efa7bf
314649558 <314649558@qq.com>
2020-07-05 16:39:01 +0800

    tree

---
+++ java-data-structs/src/main/java/com/hailong/data/tree/ArrBinaryTreeDemo.java
@@ -0,0 +1,58 @@
+package com.hailong.data.tree;
+
+/**
+ * Created by Administrator on 2020/7/5.
+ */
+public class ArrBinaryTreeDemo {
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7};
+
+        ArrBinaryTree arrBinaryTree=new ArrBinaryTree(arr);
+        arrBinaryTree.preOrder();
+    }
+}
+
+
+//编写一个ArrayBinaryTree，实现顺序存储二叉树遍历
+class ArrBinaryTree{
+    private int[] arr;//存储数据节点的数组
+
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+
+    public void preOrder(){
+        this.preOrder(0);
+    }
+
+
+    //编写一个方法，完成顺序存储二叉树的前序遍历
+
+    /**
+     * 数组的下标
+     * @param index
+     */
+    public void preOrder(int index){
+        //如果数组为空，或者arr.length==0
+        if(arr==null || arr.length==0){
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+        }
+
+        //输出当前这个元素
+        System.out.println(arr[index]);
+
+        //向左递归遍历
+        if(index*2+1<arr.length){
+            preOrder(index*2+1);
+        }
+
+        //向右递归遍历
+        if(index*2+2<arr.length){
+            preOrder(index*2+2);
+        }
+
+    }
+
+}
